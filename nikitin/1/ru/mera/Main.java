4f6a3a4a45163626165242a4179c772fcbd0baa7
n1k1t1n <nikanig@gmail.com>
2018-04-13 00:44:37 +0300

    Add files via upload

--- nikitin/1/ru/mera/Main.java
+++ nikitin/1/ru/mera/Main.java
@@ -1,1 +1,13 @@
+package ru.mera;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> strings = new ArrayList(Arrays.asList(args));
+        Collections.reverse(strings);
+        System.out.println(strings);
+    }
+}
