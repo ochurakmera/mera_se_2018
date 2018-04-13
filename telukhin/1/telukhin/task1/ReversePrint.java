4c1136ea82844b2aad1c2a43dda53f0e51024270
romaskillz <romaskillz@gmail.com>
2018-04-13 18:02:20 +0300

    Update ReversePrint.java

--- telukhin/1/telukhin/task1/ReversePrint.java
+++ telukhin/1/telukhin/task1/ReversePrint.java
@@ -1,7 +1,9 @@
 package telukhin.task1;
 
-public static void main(String[] args) {
+public class ReversePrint {
+    public static void main(String[] args) {
         for (int i = args.length - 1; i >= 0; i--) {
-            System.out.print(args[i] + " ");
+            System.out.println(args[i]);
         }
     }
+}
