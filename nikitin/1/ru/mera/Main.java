cba0a13772ad629246476fed96192e5f2fc56746
n1k1t1n <nikanig@gmail.com>
2018-04-13 16:00:16 +0300

    Update Main.java

--- nikitin/1/ru/mera/Main.java
+++ nikitin/1/ru/mera/Main.java
@@ -6,6 +6,11 @@ import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
+        //1
+        for(int i = args.length; i>=0; i--) {
+            System.out.print(args[i] + (i>0 ? ", " : '\n'));
+        }
+        //2
         ArrayList<String> strings = new ArrayList(Arrays.asList(args));
         Collections.reverse(strings);
         System.out.println(strings);
