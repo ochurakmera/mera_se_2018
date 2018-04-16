723a5fd38bb2f56aac226ed1ec92b5c5b15ffd1d
danosh <danosh@merann.ru>
2018-04-16 15:13:25 +0300

    folder fix
    Signed-off-by: danosh <danosh@merann.ru>

--- anoshkin/task1/HelloWorld.java
+++
@@ -1,6 +0,0 @@
-package anoshkin.task1;
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
--- anoshkin/task1/backOrder.java
+++
@@ -1,9 +0,0 @@
-package anoshkin.task1;
-
-class BackOrder {
-    public static void main(String[] args) {
-	for (int i=args.length-1; i>=0; i--){
-	    System.out.print(args[i] + " ");
-	}
-    }
-}
