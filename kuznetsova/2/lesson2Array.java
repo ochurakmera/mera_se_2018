08848604864c9e73558c931676f693ee0d8b654e
Kuznetsova <lkuznets@te.mera.ru>
2018-04-18 19:09:02 +0300

    Lesson2

--- kuznetsova/2/lesson2Array.java
+++ kuznetsova/2/lesson2Array.java
@@ -3,7 +3,7 @@ public class lesson2Array {
         int[] array =  {1,5,77,2,3,76,3,3,3,-2};
         int maxIndex = 0;
         for ( int i = 0; i < array.length; i++) {
-            if (array[i] > 100) {
+            if (array[i] >= 100) {
                 break;
             }
             if (array[maxIndex] < array[i]) {
--- kuznetsova/2/lesson2OneValue.java
+++ kuznetsova/2/lesson2OneValue.java
@@ -1,17 +1,15 @@
 
 public class lesson2OneValue {
     public static void main (String[] args) {
-        int value1 = 1;
+        int value1 = 9;
         int sum = 0;
 
         for ( int i=1 ; i <= value1; i++) {
             System.out.println(i);
             i++;
         }
-
-        for ( int i=0 ; i <= value1; i++) {
-            sum = i+i;
-            i++;
+        for ( int i=0 ; i <= value1; i+=2) {
+            sum += i;
         }
         System.out.println(sum);
     }
