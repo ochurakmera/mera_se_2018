c66d4bec532f086f213ca704bdb7c20eb4b47ec3
arybkin <kilkasana007@mail.ru>
2018-04-18 20:39:54 +0300

    homework 2

---
+++ rybkin/2/rybkin/src/rybkin/task2/Main2.java
@@ -0,0 +1,9 @@
+package rybkin.task2;
+
+public class Main2 {
+    public static void main(String[] args) {
+        int[] arrayInt = new int[]{1, 3, 6, 15, -2, 8, 101, 12, -3};
+        int i = 0;
+        
+    }
+}
