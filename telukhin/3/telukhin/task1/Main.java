c08ae5bdb8619bfe256fbca0b14709daba5bc9f3
Roman Telukhin <rotelukh@mera.ru>
2018-04-19 19:50:30 +0300

    Update

--- telukhin/3/telukhin/task1/Main.java
+++ telukhin/3/telukhin/task1/Main.java
@@ -3,15 +3,16 @@ package telukhin.task1;
 public class Main {
     public static void main(String[] args) {
         Author ivan = new Author("Иван", "Бунин", 1870, "Россия");
-        Author fedor = new Author("Федор", "Достоевский", 1821, "Россия");
-        Author lev = new Author("Лев", "Толстой", 1828, "Россия");
-
         ivan.setBooks(new Book("Темные аллеи", 2008, ivan, 416));
         ivan.setBooks(new Book("Митина Любовь", 2011, ivan, 352));
 
+
+        Author fedor = new Author("Федор", "Достоевский", 1821, "Россия");
         fedor.setBooks(new Book("Идиот", 2016, fedor, 640));
         fedor.setBooks(new Book("Преступление и наказание", 2016, fedor, 672));
 
+
+        Author lev = new Author("Лев", "Толстой", 1828, "Россия");
         lev.setBooks(new Book("Анна Каренина", 2017, lev, 864));
         lev.setBooks(new Book("Бегство из рая", 2017, lev, 640));
 
